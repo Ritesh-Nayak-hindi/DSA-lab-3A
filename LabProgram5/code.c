306fb3a235b18d3e1e92ee31cb66076328efbfd0
Ritesh Nayak- 1BM23EC212 <riteshmohan.ec23@bmsce.ac.in>
2024-12-06 09:10:58 +0530

    Update code.c

--- LabProgram5/code.c
+++ LabProgram5/code.c
@@ -1,82 +1,214 @@
-//Factorial using recursion
-
+//Linked List Implementation
 #include <stdio.h>
-int fact(n);
-void main()
-{
-    int n,fact1;
-    printf("Enter the number:");
-    scanf("%d",&n);
-    fact1 = fact(n);
+#include <stdlib.h> 
 
-    printf("Factorial of %d is %d",n,fact1);
+struct Node {
+    int data;
+    struct Node *link;
+};
+
+typedef struct Node node;
+node *start = NULL;
+
+void insertAtBeginning() {
+    node *new1 = (node*)malloc(sizeof(node));
+    if (new1 == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    
+    printf("Enter value to insert at beginning: ");
+    scanf("%d", &new1->data);
+    
+    new1->link = start;  
+    start = new1;    
 }
 
-int fact(n)
-{
-    if(n == 0)
-    {
-        return 1;
+void insertAtEnd() {
+    node *new1 = (node*)malloc(sizeof(node)); 
+    if (new1 == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
     }
-
-    else{
-        return n*fact(n-1);
-    }
-}
-
-//Fibonacci using recursion
-
-#include <stdio.h>
-int fib(n);
-void main()
-{
-    int n;
-    printf("Enter the number:");
-    scanf("%d",&n);
-
-    int fibo = fib(n);
-
-    printf("The fibonacci number of %d is %d",n,fibo);
-}
-
-int fib(n)
-{
-    if(n == 1)
-    {
-        return 0;
-    }
-    else if(n == 2)
-    {
-        return 1;
-    }
-    else
-    {
-        return (fib(n - 1) + fib(n - 2));
+    
+    printf("Enter value to insert at end: ");
+    scanf("%d", &new1->data);
+    
+    new1->link = NULL;
+    
+    if (start == NULL) {
+        start = new1;
+    } else {
+        node *temp = start;
+        while (temp->link != NULL) {
+            temp = temp->link;
+        }
+        temp->link = new1; 
     }
 }
 
-//Tower of hannoi using recursion
+void insertAtPosition() {
+    int pos;
+    printf("Enter the position to insert: ");
+    scanf("%d", &pos);
 
-#include <stdio.h>
+    if (pos < 1) {
+        printf("Invalid position. Position should be >= 1.\n");
+        return;
+    }
+    
+    node *new1 = (node*)malloc(sizeof(node)); 
+    if (new1 == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    
+    printf("Enter value to insert at position %d: ", pos);
+    scanf("%d", &new1->data);
+    
+    if (pos == 1) {
+        new1->link = start;
+        start = new1;
+    } else {
+        node *temp = start;
+        for (int i = 1; i < pos - 1; i++) {
+            if (temp == NULL) {
+                printf("Position out of range.\n");
+                free(new1);
+                return;
+            }
+            temp = temp->link;
+        }
 
+        new1->link = temp->link;
+        temp->link = new1;
+    }
+}
 
-void towerOfHanoi(int n, char start_rod, char to_rod, char end_rod) {
+void deleteAtBeginning() {
+    if (start == NULL) {
+        printf("The list is empty, nothing to delete.\n");
+        return;
+    }
+    
+    node *temp = start;    
+    start = start->link; 
+    free(temp); 
+    
+    printf("Node deleted from the beginning.\n");
+}
 
-    if (n == 1) {
-        printf("Move disk 1 from rod %c to rod %c\n", start_rod, to_rod);
+void deleteAtEnd() {
+    if (start == NULL) {
+        printf("The list is empty, nothing to delete.\n");
+        return;
+    }
+    
+    if (start->link == NULL) {
+        free(start);
+        start = NULL;
+        printf("Node deleted from the end.\n");
+        return;
+    }
+    
+    node *temp = start;
+    while (temp->link != NULL && temp->link->link != NULL) { // Traverse to the second last node
+        temp = temp->link;
+    }
+    
+    free(temp->link);  
+    temp->link = NULL;
+    
+    printf("Node deleted from the end.\n");
+}
+
+void deleteAtPosition() {
+    int pos;
+    printf("Enter the position to delete: ");
+    scanf("%d", &pos);
+
+  
+    if (pos < 1 || start == NULL) {
+        printf("Invalid position or list is empty.\n");
+        return;
+    }
+    
+    if (pos == 1) {
+        node *temp = start;
+        start = start->link;
+        free(temp);
+        printf("Node deleted from position 1.\n");
         return;
     }
 
-    towerOfHanoi(n - 1, start_rod, end_rod, to_rod);
+    node *temp = start;
+    for (int i = 1; i < pos - 1; i++) {
+        if (temp == NULL || temp->link == NULL) {
+            printf("Position out of range.\n");
+            return;
+        }
+        temp = temp->link;
+    }
 
-    printf("Move disk %d from rod %c to rod %c\n", n, start_rod, to_rod);
+    node *delNode = temp->link; 
+    temp->link = temp->link->link; 
+    free(delNode); 
+    
+    printf("Node deleted from position %d.\n", pos);
+}
 
-    towerOfHanoi(n - 1, end_rod, to_rod, start_rod);
+void display() {
+    node *temp;
+    if (start == NULL) {
+        printf("Linked list is empty.\n");
+        return;
+    }
+    
+    printf("Elements in the linked list: ");
+    temp = start;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->link;
+    }
+    printf("\n");
 }
 
 int main() {
-    int numOfDisks = 3;
-    printf("The sequence of moves involved in the Tower of Hanoi are:\n");
-    towerOfHanoi(numOfDisks, 'A', 'C', 'B');
+    int ch;
+    
+    while (1) {
+        printf("1 :Insert at Beginning  \n2 :Insert at End  \n3 :Insert at Position  \n4 :Delete at Beginning  \n5 :Delete at End  \n6 :Delete at Position  \n7 :Display  \n8 :Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                insertAtBeginning();
+                break;
+            case 2:
+                insertAtEnd();
+                break;
+            case 3:
+                insertAtPosition();
+                break;
+            case 4:
+                deleteAtBeginning();
+                break;
+            case 5:
+                deleteAtEnd();
+                break;
+            case 6:
+                deleteAtPosition();
+                break;
+            case 7:
+                display();
+                break;
+            case 8:
+                exit(0);  // Exit the program
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
     return 0;
 }
