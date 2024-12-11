011708506bddbd54a3a29aa5fc013eed5dd05e46
Ritesh Nayak- 1BM23EC212 <riteshmohan.ec23@bmsce.ac.in>
2024-12-11 17:35:01 +0530

    Update code.c

--- LinkedList-(Sort,Reverse,concatenete)/code.c
+++ LinkedList-(Sort,Reverse,concatenete)/code.c
@@ -1,229 +1,369 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct node
-{
+// singly linked list complete implementation
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
     int data;
-    struct node *link;
-}NODE,NODE1;
-NODE *head=NULL;
-NODE1 *head1=NULL;
-NODE* createnode()
-{
-    NODE *ptr = (NODE*)malloc(sizeof(NODE));
-    int item;
-    printf("Enter value : ");
-    scanf("%d",&item);
-    if(ptr==NULL)
-    {
-        printf("Memory not allocated");
-    }
-    else
-    {
-        ptr->data = item;
-        ptr->link=NULL;
-    }
-    return ptr;
+    struct Node *next;
+};
 
+struct Node *head1=NULL;
+struct Node *head2=NULL;
+
+struct Node * create(){
+    struct Node *temp=(struct Node *)malloc(sizeof(struct Node));
+    if(!temp){
+        return NULL;
+    }
+    printf("ENTER THE DATA THAT NEEDS TO BE INSERTED");
+    scanf("%d",&temp->data);
+    temp->next=NULL;
+    return temp;
 }
 
-void insert_beg()
-{
-    NODE *ptr=createnode();
-    if(head==NULL)
-    {
-        head=ptr;
-    }
-    else if(head!=NULL)
-    {
-        ptr->link=head;
-        head=ptr;
-    }
-}
-void insert_end()
-{
-    NODE *ptr=createnode();
-    NODE *temp;
-    temp = head;
-    while(temp->link!=NULL)
-    {
-        temp=temp->link;
-    }
-    temp->link=ptr;
-}
-void display()
-{
-    NODE *temp;
-    temp=head;
-    if(head==NULL)
-    {
-        printf("List is empty");
+void insert_at_pos1(){
+    struct Node *temp=create();
+    if(head1==NULL){
+        head1=temp;
         return;
     }
-    else
-    {
-        printf("List elements are : \n");
+    int pos;
+    printf("ENTER THE POSITION AT WHICH YOU WOULD LIKE TO INSERT");
+    scanf("%d",&pos);
+    if(pos<=0){
+        printf("THE ENTERED POSTION MUST BE NON ZERO AND POSITIVE");
+        return;
     }
-    while(temp!=NULL)
-    {
-        printf("%d\t",temp->data);
-        temp = temp->link;
+    if(pos==1){
+        temp->next=head1;
+        head1=temp;
+        return;
+    }
+    int i=1;
+    struct Node *curr=head1;
+    while(i<pos-1 && curr->next!=NULL){
+        curr=curr->next;
+        i++;
+    }
+    
+    if(i<pos-1){
+        printf("INVALID POSITION ENTERED");
+        return;
+    }
+    temp->next=curr->next;
+    curr->next=temp;
+    
+    
+}
+void insert_at_pos2(){
+    struct Node *temp=create();
+    if(head2==NULL){
+        head2=temp;
+        return;
+    }
+    int pos;
+    printf("ENTER THE POSITION AT WHICH YOU WOULD LIKE TO INSERT");
+    scanf("%d",&pos);
+    if(pos<=0){
+        printf("THE ENTERED POSTION MUST BE NON ZERO AND POSITIVE");
+        return;
+    }
+    if(pos==1){
+        temp->next=head2;
+        head2=temp;
+        return;
+    }
+    int i=1;
+    struct Node *curr=head2;
+    while(i<pos-1 && curr->next!=NULL){
+        curr=curr->next;
+        i++;
+    }
+    
+    if(i<pos-1){
+        printf("INVALID POSITION ENTERED");
+        return;
+    }
+    temp->next=curr->next;
+    curr->next=temp;
+    
+    
+}
+
+void delete_at_pos1(){
+    if(head1==NULL){
+        printf("THE GIVEN LIST IS ALREADY EMPTY");
+        return;
+    }
+    
+    printf("ENTER THE POSITION AT WHICH YOU WOULD LIKE TO DELETE");
+    int pos;
+    scanf("%d",&pos);
+    if(pos<=0){
+        printf("THE VALUE OF THE ENTERED POSITION SHOULD BE GREATER THAN ZERO");
+        return;
+    }
+    if(pos==1){
+        struct Node *curr=head1;
+        head1=head1->next;
+        curr->next=NULL;
+        free(curr);
+        return;
+    }
+    struct Node *curr=head1;
+    struct Node *currp=head1;
+    int i=0;
+    while(i<pos-1 && curr->next!=NULL){
+        currp=curr;
+        curr=curr->next;
+        i++;
+    }
+    if(i<pos-1){
+        printf("INVALID POSITION VALUE");
+        return;
+    }
+    currp->next=curr->next;
+    curr->next=NULL;
+    free(curr);
+    
+    
+}
+void delete_at_pos2(){
+    if(head2==NULL){
+        printf("THE GIVEN LIST IS ALREADY EMPTY");
+        return;
+    }
+    
+    printf("ENTER THE POSITION AT WHICH YOU WOULD LIKE TO DELETE");
+    int pos;
+    scanf("%d",&pos);
+    if(pos<=0){
+        printf("THE VALUE OF THE ENTERED POSITION SHOULD BE GREATER THAN ZERO");
+        return;
+    }
+    if(pos==1){
+        struct Node *curr=head2;
+        head2=head2->next;
+        curr->next=NULL;
+        free(curr);
+        return;
+    }
+    struct Node *curr=head2;
+    struct Node *currp=head2;
+    int i=0;
+    while(i<pos-1 && curr->next!=NULL){
+        currp=curr;
+        curr=curr->next;
+        i++;
+    }
+    if(i<pos-1){
+        printf("INVALID POSITION VALUE");
+        return;
+    }
+    currp->next=curr->next;
+    curr->next=NULL;
+    free(curr);
+    
+    
+}
+
+struct Node * concate(struct Node * L1,struct Node *L2){
+    struct Node *temp1=L1;
+   
+    if(L1==NULL && L2==NULL){
+        return NULL;
+    }
+    if(L1==NULL && L2){
+        return L2;
+    }
+    if(L2==NULL && L1){
+        return L1;
+    }
+    while(temp1->next!=NULL){
+        temp1=temp1->next;
+    }
+    temp1->next=L2;
+    return L1;
+}
+
+void search(struct Node *L1){
+    if(L1==NULL){
+        printf("THE GIVEN LINKED LIST IS COMPLETLEY EMPTY NO POINT OF SEARCHING\n");
+        return;
+    }
+    printf("ENTER THE ELEMENT TO BE SEARCHED");
+    int flag=-1;
+    int ele;
+    scanf("%d",&ele);
+    struct Node *curr=L1;
+    if(curr->data==ele){
+        printf("THE ELEMENT IS FOUND");
+        return;
+    }
+    while(curr->next!=NULL){
+        if(curr->data==ele){
+            flag=10;
+            return;
+        }
+        curr=curr->next;
+    }
+    if(flag!=-1){
+        printf("THE ELEMENT HAS BEEN FOUND");
+    }
+    else{
+        printf("THE ELEMENT HAS NOT BEEN FOUND");
     }
 }
-void sort()
-{
-    NODE *start = head;
-    while(start!=NULL)
-    {
-        NODE *temp=start->link;
-        while(temp!=NULL)
-        {
-            if(start->data > temp->data)
-            {
-                int x=start->data;
-                start->data=temp->data;
-                temp->data=x;
+
+void sort(struct Node *L1){
+    if(L1==NULL || L1->next==NULL){
+        printf("THE ENTERD LIST IS EMPTY");
+        return;
+    }
+    struct Node *curr=L1;
+    
+    while(curr!=NULL){
+        struct Node *currn=curr->next;
+        while(currn!=NULL){
+            if(curr->data>currn->data){
+                int temp=curr->data;
+                curr->data=currn->data;
+                currn->data=temp;
             }
-            temp=temp->link;
+            currn=currn->next;
         }
-        start=start->link;
+        curr=curr->next;
     }
-   
+    
+    
 }
-void reverse()
-{
-    NODE* prev=NULL;
-    NODE* next = NULL;
-    NODE* curr = head;
-    while(curr!=NULL)
-    {
-        next=curr->link;
-        curr->link=prev;
-        prev = curr;
-        curr=  next;
-    }
-    head = prev;
+struct Node * merge(struct Node *L1,struct Node *L2){
+    struct Node *temp1=concate(L1, L2);
+    L1=temp1;
+    sort(L1);
+    return L1;
 }
-NODE1* createnode1()
-{
-    NODE1 *ptr = (NODE1*)malloc(sizeof(NODE1));
-    int item;
-    printf("Enter value FOR NODE 2 : ");
-    scanf("%d",&item);
-    if(ptr==NULL)
-    {
-        printf("Memory not allocated");
-    }
-    else
-    {
-        ptr->data = item;
-        ptr->link=NULL;
-    }
-    return ptr;
 
-}
-void insert_beg1()
-{
-    NODE1 *ptr=createnode1();
-    if(head1==NULL)
-    {
-        head1=ptr;
-    }
-    else if(head1!=NULL)
-    {
-        ptr->link=head1;
-        head1=ptr;
-    }
-}
-void insert_end1()
-{
-    NODE1 *ptr=createnode1();
-    NODE1 *temp;
-    temp = head1;
-    while(temp->link!=NULL)
-    {
-        temp=temp->link;
-    }
-    temp->link=ptr;
-}
-void display1()
-{
-    NODE1 *temp;
-    temp=head1;
-    if(head1==NULL)
-    {
-        printf("List is empty");
+void revrese(struct Node **L1){
+    if(L1==NULL){
+        printf("THE GIVEN LIST IS EMPTY");
         return;
     }
-    else
-    {
-        printf("List elements are : \n");
+    struct Node *curr=*L1;
+    struct Node *next=NULL;
+    struct Node *pre=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=pre;
+        pre=curr;
+        curr=next;
     }
-    while(temp!=NULL)
-    {
-        printf("%d\t",temp->data);
-        temp = temp->link;
+    *L1=pre;
+}
+
+void display(struct Node *L1){
+    if(L1==NULL){
+        printf("THE GIVEN LIST IS EMPTY!!");
+        return;
+    }
+    struct Node *curr=L1;
+    while(curr!=NULL){
+        printf("%d ",curr->data);
+        curr=curr->next;
     }
 }
-void extra()
-{
-    int choice=0;
-    while(choice<=5)
-    {
-        printf("\nENTER CHOICE : \n1.INSERT AT BEG\n2.INSERT END\n3.DISPLAY\n4.CONCATENATE\n");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1:insert_beg1();
-                    break;
-            case 2:insert_end1();
-                    break;
-            case 3:display1();
-                    break;
-            case 4:concatenate();
-                    break;
+
+int main(){
+    while(1){
+        int i;
+        printf("ENTER YOUR CHOICE\n1.insert\n2.delete\n3.concatenate\n4.search\n5.display\6.merge the lists\n7.sort the list\n8.reverse the linked list\n-1 to exit ");
+        scanf("%d",&i);
+        if(i==-1){
+            exit(0);
         }
-       
-    }
-}
-void concatenate()
-{
-   NODE *temp = head;
-   NODE *temp1=head1;
-    while(temp->link!=NULL)
-    {
-        temp=temp->link;
-    }
-    temp->link=head1;
-    temp=head;
-    while(temp!=NULL)
-    {
-        printf("%d\t",temp->data);
-        temp=temp->link;
-    }
-}
-void main()
-{
-    int choice=0;
-   
-    while(choice<=5)
-    {
-        printf("\nENTER CHOICE : \n1.INSERT AT BEG\n2.INSERT END\n3.DISPLAY\n4.SORT\n5.REVERSE\n6.CONCATENATE\n");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1:insert_beg();
-                    break;
-            case 2:insert_end();
-                    break;
-            case 3:display();
-                    break;
-            case 4:sort();
-                    break;
-            case 5:reverse();
-                    break;
-            case 6:printf("Enter node 2 \n");
-                    extra();
-                    break;
+        switch(i){
+            case 1:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i;
+                scanf("%d",&i);
+                if(i==1){
+                    insert_at_pos1();
+                }
+                else{
+                    insert_at_pos2();
+                }
+                break;
+            case 2:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i1;
+                scanf("%d",&i1);
+                if(i1==1){
+                    delete_at_pos1();
+                }
+                else{
+                    delete_at_pos2();
+                }
+                break;
+            case 3:
+                head1=concate(head1, head2);
+                display(head1);
+                break;
+            case 4:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i3;
+                scanf("%d",&i3);
+                if(i3==1){
+                    search(head1);
+                }
+                else{
+                    search(head2);
+                }
+                break;
+            case 5:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i2;
+                scanf("%d",&i2);
+                if(i2==1){
+                    display(head1);
+                }
+                else{
+                    display(head2);
+                }
+                break;
+            case 6:
+                head1=merge(head1, head2);
+                display(head1);
+                break;
+            case 7:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i5;
+                scanf("%d",&i5);
+                if(i5==1){
+                    sort(head1);
+                }
+                else{
+                    sort(head2);
+                }
+                break;
+            case 8:
+                printf("ENTER THE WHICH LINKED LIST YOU WNAT TO INSERT 1 or 2");
+                int i6;
+                scanf("%d",&i6);
+                if(i6==1){
+                    revrese(&head1);
+                }
+                else{
+                    revrese(&head2);
+                }
+                break;
+            default:
+                printf("THE ENTERED CHOICE IS INVALID");
+                break;
+                
+        
         }
+        
     }
+    
 }
+
+
